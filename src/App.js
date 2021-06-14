265674d225a04c02125fd07bff56c46a465d798b
ia7medd <ahmed.salama59@gmail.com>
2021-06-14 23:45:43 +0400

    test

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Latest update
         </a>
       </header>
     </div>
